8332a93a85332a7c918e5653719884f9533f028d
henrrycoronado <126266756+henrrycoronado@users.noreply.github.com>
2026-03-14 10:08:17 -0400

    chore(db): add models

---
+++ InventarySystem.Api/src/Modules/Sales/Infrastructure/SaleStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventarySystem.Api.Modules.Sales.Infrastructure;
+
+public partial class SaleStatus
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public bool? IsActive { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();
+}
